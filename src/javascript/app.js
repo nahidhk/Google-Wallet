37e402cae12fc74839e3e25fe3c3288625556247
Nahid HK <nahidappltd@gmail.com>
2025-05-14 00:27:15 +0600

    new update

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,7 +1,7 @@
 const jsonData = 
 {
   "sheet": "https://docs.google.com/spreadsheets/d/1fiZzJ1IVEg6ebzMXyp8ZQZe9IiEizJVZXFNvY3xslA0/edit?usp=drivesdk",
-  "jsonApi": "https://script.google.com/macros/s/AKfycbxs5eklxHQ-aRzwxaJzFcP9XE36Cr7NVt_6CCy8fU-8517xTjV7Qo9SsUO9_WT0Rp1v/exec",
+  "jsonApi": "https://script.google.com/macros/s/AKfycbzSqwwhAZdgZWJvj8dag1MRy9I5Ykbwzy01fyPz2nhrCVFbzpn3r5J_6dF8WFpePDSX/exec",
   "submitFormUrl":"Past The HTML Form",
   "number":"01877357091",
   "password":"NAHID12345",
@@ -16,61 +16,11 @@ const jsonData =
 
 
 
+
 const apiurl = jsonData.jsonApi;
 const today = new Date().toISOString().split('T')[0];
 document.getElementById('autodate').value = today; 
 document.getElementById('autodate1').value = today; 
-
-async function displayData() {
-  try {
-    const response = await fetch(apiurl);
-    const data = await response.json();
-    const dataContainer = document.getElementById("app");
-
-    if (!dataContainer) {
-      throw new Error("Element with id 'app' not found.");
-    }
-
-    let totalCredit = 0;
-    let totalDebit = 0;
-
-    data.innerHTML = "";
-    data.forEach((item) => {
-      totalDebit += parseFloat(item.debit) || 0;
-      totalCredit += parseFloat(item.credit) || 0;
-
-      const itemElement = document.createElement("div");
-      itemElement.innerHTML = `
- <div class="databox">
-        <div class="flex beet">
-          <div>
-            <h2>${item.type}</h2>
-            <p class="p"> ${item.details} </p>
-            <p class="p">${new Date(item.date).toLocaleDateString("en-GB")}</p>
-          </div>
-          <div class="px">
-            <br />
-            <h1 >${item.credit.toLocaleString('en-US', { minimumFractionDigits: 2 })}${item.debit.toLocaleString('en-US', { minimumFractionDigits: 2 })}</h1>
-          </div>
-        </div>
-        </div>
-      `;
-      dataContainer.appendChild(itemElement);
-    });
-
-    loadapp('none');
-    
-    const netTotal = totalCredit - totalDebit;
-    
-   document.getElementById('taka').innerHTML=netTotal.toLocaleString('en-US', { minimumFractionDigits: 2 });
-    
-    
-  } catch (error) {
-    console.error("data error", error);
-  }
-}
-
-displayData();
 
 function sheets(){
   window.location.href=jsonData.sheet;
@@ -102,3 +52,57 @@ function reloadapp() {
   window.location.reload();
 }
 
+
+
+
+
+//  dtaa
+
+
+
+
+async function displayData() {
+  try {
+    const response = await fetch(apiurl);
+    const data = await response.json();
+    const dataContainer = document.getElementById("app");
+
+    if (!dataContainer) {
+      throw new Error("Element with id 'app' not found.");
+    }
+
+    let totalCredit = 0;
+    let totalDebit = 0;
+
+    data.innerHTML = "";
+    data.forEach((item) => {
+      totalDebit += parseFloat(item.debit) || 0;
+      totalCredit += parseFloat(item.credit) || 0;
+
+      const itemElement = document.createElement("tr");
+      itemElement.innerHTML = `
+  <td>${new Date(item.Date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  })}</td>
+  <td class="right">${item.Basic.toLocaleString('en-US', { minimumFractionDigits: 2 })}</td>
+  <td class="right">${item.Bonus.toLocaleString('en-US', { minimumFractionDigits: 2 })}</td>
+`
+;
+      dataContainer.prepend(itemElement);
+    });
+
+    loadapp('none');
+    
+    const netTotal = totalCredit - totalDebit;
+    
+   document.getElementById('taka').innerHTML=netTotal.toLocaleString('en-US', { minimumFractionDigits: 2 });
+    
+    
+  } catch (error) {
+    console.error("data error", error);
+  }
+}
+
+displayData();
