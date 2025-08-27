a51b245aa20cbfb48812eb189c7e0f40ade2fab5
nahidhk <nahidappltd@gmail.com>
2025-08-27 09:32:08 +0600

    new updatex

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Nav /> <!-- Nav component use করা হলো -->
+    <h1>Main App Content</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Nav from './components/Nav.vue'  // Import করা হলো
+
+export default {
+  name: 'App',
+  components: {
+    Nav  // Declare করা হলো
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+/* Optional global styles */
 </style>
+
+
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h3 class="mrgin5px fff">
+        Google Wallet
+      </h3>
+
+      <div class="blanceBox">
+        <i class="fa-solid fa-bangladeshi-taka-sign"></i> <span id="taka"><i class="fa-solid fa-arrow-rotate-right fa-spin"></i></span>
+        TK
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
